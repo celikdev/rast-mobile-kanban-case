c55aa99634f1b23a574251d3845571e56c7686e3
Hakan <52114496+celikdev@users.noreply.github.com>
2024-06-19 01:00:57 +0300

    edit: add logger page button

--- src/app/board/[id]/page.tsx
+++ src/app/board/[id]/page.tsx
@@ -5,6 +5,7 @@ import { useGetBoard } from "@/hooks/useBoard";
 import { useMoveTask } from "@/hooks/useTask";
 import { createLog } from "@/services/log";
 import { DndContext } from "@dnd-kit/core";
+import Link from "next/link";
 import { HashLoader } from "react-spinners";
 
 // Açıklama: Sayfa URL içindeki parametreyi kullanarak board detaylarını getirir.
@@ -30,7 +31,17 @@ const BoardDetail = ({ params }: { params: { id: string } }) => {
           </div>
         ) : (
           <>
-            <h1 className="text-4xl font-black text-teritary">{data.title}</h1>
+            <span className="flex items-center w-full justify-center">
+              <h1 className="text-4xl font-black text-teritary">
+                {data.title}
+              </h1>
+              <Link
+                href="/logs"
+                className="border-2 rounded-lg p-4 text-sm font-semibold text-teritary hover:bg-teritary hover:text-secondary transition-colors duration-300 ml-4"
+              >
+                Visit Logs
+              </Link>
+            </span>
             <div className="w-full h-[100%]">
               <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 w-full gap-10 px-10 h-[100%]">
                 {data.lists.map((list: List) => (
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
 import { BoardList } from "@/components/board";
+import Link from "next/link";
 
 const Home = () => {
   return (
     <main className="flex flex-col items-center py-10 gap-4 h-[100%]">
-      <h1 className="text-6xl font-black text-teritary">Board List</h1>
+      <span className="flex items-center w-full justify-center">
+        <h1 className="text-6xl font-black text-teritary">Board List</h1>
+        <Link
+          href="/logs"
+          className="border-2 rounded-lg p-4 text-sm font-semibold text-teritary hover:bg-teritary hover:text-secondary transition-colors duration-300 ml-4"
+        >
+          Visit Logs
+        </Link>
+      </span>
       <BoardList />
     </main>
   );
