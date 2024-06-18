2e32d74ff4214c956bb5b5d4d397fef894c54a9d
Hakan <52114496+celikdev@users.noreply.github.com>
2024-06-19 01:07:12 +0300

    fix: localstorage error

--- src/services/log/index.ts
+++ src/services/log/index.ts
@@ -1,23 +1,29 @@
-"use client"
-
 // Add Local Storage Log
 export const getLogs = () => {
-    const logs = localStorage.getItem("logs");
-    if (logs) {
-        return JSON.parse(logs);
+    if (process.browser) {
+        if (localStorage !== undefined) {
+            const logs = localStorage.getItem("logs");
+            if (logs) {
+                return JSON.parse(logs);
+            }
+            return [];
+        }
     }
-    return [];
 }
 
 const createLog = async (log: Log) => {
-    const logs = getLogs();
-    const newLog = {
-        message: log.message,
-        createdAt: new Date().toISOString(),
-        _id: Math.random().toString(36).substr(2, 9),
+    if (process.browser) {
+        if (localStorage !== undefined) {
+            const logs = getLogs();
+            const newLog = {
+                message: log.message,
+                createdAt: new Date().toISOString(),
+                _id: Math.random().toString(36).substr(2, 9),
+            }
+            logs.push(newLog);
+            localStorage.setItem("logs", JSON.stringify(logs));
+        }
     }
-    logs.push(newLog);
-    localStorage.setItem("logs", JSON.stringify(logs));
 }
 
 
